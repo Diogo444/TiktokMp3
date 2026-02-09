92d01d70e0533254a76a075bca5f19efb86a9e00
Diogo Andrade <da7822000@gmail.com>
2026-02-09 22:03:34 +0100

    fix 502

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -50,9 +50,15 @@ const setNoStoreHeaders = (res) => {
   res.setHeader('Expires', '0');
 };
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+};
+
 // Middleware
-app.use(cors());
-app.options('*', cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -64,6 +70,9 @@ app.use((req, res, next) => {
   // Headers de cache pour les APIs
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
 
   if (req.path.startsWith('/api/')) {
     setNoStoreHeaders(res);
